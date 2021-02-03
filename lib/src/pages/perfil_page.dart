87021bb6798d7269dc0e1a5a645ad8011a615931
Richard Pumapillo <richard.pumapillo@unmsm.edu.pe>
2021-02-03 18:51:27 -0500

    se agrego el provider de perfil

--- lib/src/pages/perfil_page.dart
+++ lib/src/pages/perfil_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:app_matricula_proy_final/src/bloc/provider.dart';
 import 'package:app_matricula_proy_final/src/widget/app_drawer.dart';
 
 class PerfilPage extends StatefulWidget {
@@ -12,12 +13,21 @@ class PerfilPage extends StatefulWidget {
 class _PerfilPageState extends State<PerfilPage> {
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Perfil'),
-      ),
-      drawer: AppDrawer(),
-      body: Text('cuerpo de perfil'),
-    );
+        appBar: AppBar(
+          title: Text('Perfil'),
+        ),
+        drawer: AppDrawer(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Email: ${bloc.email}'),
+            Divider(),
+            Text('Password: ${bloc.password}')
+          ],
+        ));
   }
 }
