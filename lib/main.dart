e2d7b206af0ba4c582752207ce9f9bb3655c0ba5
nielhuaman <73756567+nielhuaman@users.noreply.github.com>
2021-02-03 22:25:51 -0500

    1
    1

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ import 'package:app_matricula_proy_final/src/pages/maestria_cursos.dart';
 import 'package:app_matricula_proy_final/src/pages/login_page.dart';
 import 'package:app_matricula_proy_final/src/pages/producto_page.dart';
 import 'package:app_matricula_proy_final/src/pages/registro_page.dart';
+import 'package:app_matricula_proy_final/src/pages/matricula_page.dart';
+import 'package:app_matricula_proy_final/src/pages/matricula_new.dart';
 import 'package:app_matricula_proy_final/src/preferencias_usuario/preferencias_usuario.dart';
 
 void main() async {
@@ -44,7 +46,12 @@ class MyApp extends StatelessWidget {
           'perfil': (BuildContext context) => PerfilPage(),
           'Maestrias': (BuildContext context) => MaestriasPage(),
           'NewMaestrias': (BuildContext context) => MaestriasNew(),
+<<<<<<< HEAD
           'Maestria_Cursos': (BuildContext context) => Maestria_CursoPage(),
+=======
+          'Matricula': (BuildContext context) => MatriculaPage(),
+          'NewMatricula': (BuildContext context) => MatriculaNew()          
+>>>>>>> bf6c50dbc9912bd871e82c49ab4acbde7e12a051
         },
         theme: ThemeData(
           primaryColor: Colors.blue[700],
